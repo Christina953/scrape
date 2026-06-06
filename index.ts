6da5c8eb0b6e091d92abddf0f41534ee4414d3ba
Christina Vahi <christina.vahi@tptlive.ee>
2026-06-06 16:00:21 +0300

    Add simple scraper

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import * as cheerio from 'cheerio';
+
+let res = await fetch('https://xkcd.com/3253/');
+let html = await res.text();
+const $ = cheerio.load(html);
+let img = $('#comic>img');
+console.log(img.attr('src'));
+console.log(img.attr('title'));
+console.log(img.attr('alt'));
